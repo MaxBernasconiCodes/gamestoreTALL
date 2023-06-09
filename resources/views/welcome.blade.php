312ef430dfa8ec1c6988b5368d470ebe1fa7876e
MaxBernasconi <max.bernasconi86@gmail.com>
2023-06-09 19:48:09 -0300

    style correction on index and banner

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="relative min-h-screen min-vw-100 flex flex-col justify-between items-center bg-black  text-white">
         <livewire:components.nav-bar />
-        <div class="w-full flex flex-col">
+        <div class="w-full mb-auto flex flex-col justify-start items-start gap-4">
             <livewire:components.banner />
             <livewire:game.index />
         </div>
