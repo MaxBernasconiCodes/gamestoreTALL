df7871ede3f4dd28d75439dd66fc182193704779
MaxBernasconi <max.bernasconi86@gmail.com>
2023-06-09 20:37:07 -0300

    class correction

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-   <div class="relative min-h-screen flex flex-col justify-between items-center text-white">
+   <div class="relative min-h-screen flex flex-col justify-between items-center bg-black text-white">
     <livewire:components.nav-bar>
     <h2 class=" text-pink-500 font-bold text-center text-7xl "> 404 Page not Found </h1>
         <img  class="w-full" src="https://onextrapixel.com/wp-content/uploads/2018/11/Mario-Bros-404-by-Miles-Peebles.jpg" alt="Page not Found">
