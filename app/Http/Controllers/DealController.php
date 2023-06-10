4421bfc403ce06bdb539bb4f93febc9f3b16ba3e
MaxBernasconi <max.bernasconi86@gmail.com>
2023-06-10 17:18:23 -0300

    DealController created, index geting data and req

--- app/Http/Controllers/DealController.php
+++ app/Http/Controllers/DealController.php
@@ -8,10 +8,18 @@ use Illuminate\Support\Facades\Storage;
 class DealController extends Controller
 {
     public function index(Request $request){
+        //request handling
         $searchParameters = explode(",", $request->input('q'));
-        dd($searchParameters);
+
+        //geting data from JSON file
         $data = Storage::get('data/deals.json');
         $deals = json_decode($data, true);
+
+        //determining filters
+        // -checking if filters are valid
+        // -aplying filters to the data
+
+        //returning filtered data
         return  $deals;
     }
 }
